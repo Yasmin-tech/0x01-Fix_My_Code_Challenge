28e5457519f88689bf3cbefca0a2508470c2df82
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-04-15 03:20:32 +0300

    fix the square

--- square.py
+++ square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
 class square():
-    
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
+        if (self.width != self.height):
+            self.height = self.width
 
     def area_of_my_square(self):
         """ Area of the square """
@@ -20,8 +21,8 @@ class square():
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
